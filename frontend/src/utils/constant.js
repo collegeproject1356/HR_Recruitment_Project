87bd9531b958ff664c37efadccde55298f0e9b34
collegeproject1356 <collegeproject1356@gmail.com>
2026-03-19 10:11:04 +0530

    commit

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-// Ye hamare backend server ka main address hai
-export const USER_API_END_POINT = "http://localhost:5000/api/users";
-export const JOB_API_END_POINT = "http://localhost:5000/api/jobs";
-export const APPLICATION_API_END_POINT = "http://localhost:5000/api/applications";
+export const USER_API_END_POINT = "http://hr-recruitment-project-blond.vercel.app/api/users";
+export const JOB_API_END_POINT = "http://hr-recruitment-project-blond.vercel.app/api/jobs";
+export const APPLICATION_API_END_POINT = "http://hr-recruitment-project-blond.vercel.app/api/applications";
+
